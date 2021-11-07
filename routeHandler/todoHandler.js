7bbbb5f428c57f6934d43b0dbdf2317480547d96
Muktar <muktar.webdev@gmail.com>
2021-11-07 11:15:57 +0600

    Instance Methods,Static & Query Helpers

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -4,28 +4,38 @@ const router = express.Router();
 const todoSchema = require("../schemas/todoSchema");
 
 // -- crateing Mongoose model based on Schema
-const Todo = mongoose.model("Todo", todoSchema);
+const Todo = new mongoose.model("Todo", todoSchema);
+
+//  instance methode
+router.get("/active", async (req, res) => {
+  const todo = new Todo();
+  const data = await todo.findActive();
+  res.status(200).json({
+    data: data,
+  });
+});
+
+// static methods
+router.get("/js", async (req, res) => {
+  const data = await Todo.findByJS();
+  res.status(200).json({ data: data });
+});
+
+// query helpers
+router.get("/language", async (req, res) => {
+  // const data = await Todo.find().byLanguage("JS");
+  const data = await Todo.find().byLanguage("React");
+  res.status(200).json({ data });
+});
 
 // GET all the Todos
-router.get("/", async (req, res) => {
-  /* // for finding the all todos (database all collectiions of documents with all filed)
-  await Todo.find({ status: "active" }, (err, data) => {
-    if (err) {
-      res.status(500).json({ error: "Server Side Problem" });
-    } else {
-      res.status(200).json({
-        result: data,
-        message: "Success!",
-      });
-    }
-  }); */
-
-  // for finding the all todos (database all collections of documents with particular field)
-  await Todo.find({ status: "active" })
+router.get("/", (req, res) => {
+  Todo.find({ status: "active" })
     .select({
       _id: 0,
       date: 0,
     })
+    .limit(2)
     .exec((err, data) => {
       if (err) {
         res.status(500).json({ error: "Server Side Problem" });
@@ -40,19 +50,6 @@ router.get("/", async (req, res) => {
 
 // GET a single todo by ID
 router.get("/:id", async (req, res) => {
-  // way-01
-  /* await Todo.findOne({ _id: req.params.id }, (err, data) => {
-    if (err) {
-      res.status(500).json({ error: "Server Side Problem" });
-    } else {
-      res.status(200).json({
-        result: data,
-        message: "Success!",
-      });
-    }
-  }); */
-
-  // way-02
   try {
     const data = await Todo.findOne({ _id: req.params.id });
     res.status(200).json({ data: data, message: "Success" });
@@ -63,21 +60,6 @@ router.get("/:id", async (req, res) => {
 
 // insert single todo
 router.post("/", async (req, res) => {
-  /* // way -01
-  const newTodo = new Todo(req.body); // using the Mngoose model
-  await newTodo.save((err) => {
-    if (err) {
-      res.status(500).json({
-        error: "There was a server side Problem!",
-      });
-    } else {
-      res.status(200).json({
-        message: "Todo was inserted succesfully!!",
-      });
-    }
-  });  */
-
-  // way -02
   try {
     const newTodo = new Todo(req.body);
     await newTodo.save();
